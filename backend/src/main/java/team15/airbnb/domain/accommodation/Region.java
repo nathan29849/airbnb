baec21b57a4bbe9a5271aa57d93e9b516e8a25e5
nathan29849 <phs960418@gmail.com>
2022-06-01 14:57:42 +0900

    refactor: [49]좌표를 double로 관리하지 않고 Spatial type인 Point로 관리 - coordinate

--- backend/src/main/java/team15/airbnb/domain/accommodation/Region.java
+++ backend/src/main/java/team15/airbnb/domain/accommodation/Region.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import org.locationtech.jts.geom.Point;
 
 @Entity
 public class Region {
@@ -23,10 +24,6 @@ public class Region {
 	private String image;
 
 	@NotNull
-	@Column(name = "region_latitude")
-	private double latitude;
-
-	@NotNull
-	@Column(name = "region_longitude")
-	private double longitude;
+	@Column(columnDefinition = "point")
+	private Point coordinate;
 }
